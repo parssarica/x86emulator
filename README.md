6fe8113e88323ee34f74e76994cd04e7298efc27
Pars SARICA <parssarica@gmail.com>
2026-03-24 23:50:40 +0300

    ud2 instruction implemented.

--- README.md
+++ README.md
@@ -72,7 +72,7 @@ Toggle commands:
 5. registers		Toggles showing registers.
 
 ##  Total number of instructions and syscalls
-Total instruction count: 162
+Total instruction count: 163
 
 SIMD instruction count: 28
 
--- emu.py
+++ emu.py
@@ -2819,6 +2819,8 @@ while True:
                 raise Exception(f"Error: RIP is {hex(get_register_value("rip"))}. Can't operate on immediates.")
 
             set_register_value(arg1, bin(get_register_value(arg2)).replace("0b", "").zfill(get_register_bits(arg2)).count("0"))
+        case "ud2":
+            raise Exception(f"Error RIP is {hex(get_register_value("rip"))}. Invalid Opcode Exception.")
         case "endbr64" | "nop" | "nopl" | "nopw" | "notrack" | "prefetcht0" | "prefetcht1" | "prefetcht2" | "prefetcht3" | "prefetchnta" | "sfence" | "prefetchw" | "":
             pass
         case _:
